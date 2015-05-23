409736374df0d95e1ca77d04f3fe99cc81dabe1b
leehkl <slee.hkl@gmail.com>
2015-05-23 12:57:33 -0700

    User Account Page

---
+++ php/db.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+	$host = 'localhost';
+	$user = 'ericiann_pawcon';
+	$pwd = 'pawconnections';
+	$db = 'ericiann_pawconnections';
+
+//new database
+$mysqli = new mysqli($host, $user, $pwd, $db);
+if(!$mysqli || $mysqli->connect_errno){
+  echo "Connection Error" . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+?>
